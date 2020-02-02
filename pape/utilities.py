8db14737a44ca9a492c3503bf273940dd92cac46
Carter Pape <carterpape@gmail.com>
2020-02-02 01:30:08 -0700

    adds pluralize

--- pape/utilities.py
+++ pape/utilities.py
@@ -6,16 +6,29 @@
 from math import floor
 
 _typical_prefix_map = {
-    0   :   "th",
-    1   :   "st",
-    2   :   "nd",
-    3   :   "rd",
-    4   :   "th",
-    5   :   "th",
-    6   :   "th",
-    7   :   "th",
-    8   :   "th",
-    9   :   "th",
+    0: "th",
+    1: "st",
+    2: "nd",
+    3: "rd",
+    4: "th",
+    5: "th",
+    6: "th",
+    7: "th",
+    8: "th",
+    9: "th",
+}
+
+_ap_number_replacements_map = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
 }
 
 def ordinal(number: int) -> str:
@@ -29,3 +42,19 @@ def ordinal(number: int) -> str:
 
 def _is_teenthish(number: int) -> bool:
     return (floor(number / 10) % 10) == 1
+
+def pluralize(*,
+    singular_form: str,
+    count,
+    plural_form = None,
+    use_AP_style = True
+) -> str:
+    if plural_form == None:
+        plural_form = f"{singular_form}s"
+        
+    correct_form = singular_form if count == 1 else plural_form
+    
+    if use_AP_style and count in _ap_number_replacements_map:
+        count = _ap_number_replacements_map[count]
+    
+    return f"{count} {correct_form}"
